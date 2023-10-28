995b1934343da34eb0871ad78bb24709de62ba6a
Maria Zhuravleva <contact.mzhuravleva@gmail.com>
2023-10-28 09:21:07 -0400

    refactor home page to use template

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 class Finch:  
   def __init__(self, name, breed, description, age):
@@ -17,7 +17,7 @@ finches = [
 
 
 def home(request):
-  return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
