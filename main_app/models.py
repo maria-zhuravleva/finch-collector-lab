f8c44801978b0632e26fb181a0d69af0e57da769
Maria Zhuravleva <contact.mzhuravleva@gmail.com>
2023-10-28 09:56:13 -0400

    add css style to home page

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Finch(models.Model):
+  name = models.CharField(max_length=100)
+  species = models.CharField(max_length=100)
+  color = models.CharField(max_length=50)
+  wingspan = models.FloatField()
+  lifespan = models.IntegerField()
+  is_migratory = models.BooleanField(default=False)
