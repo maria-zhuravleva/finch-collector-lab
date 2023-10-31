781ce8d2bbeb7780b4f123d6fed4a3a8aaa9bbe3
Maria Zhuravleva <contact.mzhuravleva@gmail.com>
2023-10-31 08:11:52 -0400

    create Feeding model

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.urls import reverse
 
+MEALS = (
+  ('B', 'Breakfast'),
+  ('L', 'Lunch'),
+  ('D', 'Dinner')
+)
 class Finch(models.Model):
   name = models.CharField(max_length=100)
   species = models.CharField(max_length=100)
@@ -14,3 +19,16 @@ class Finch(models.Model):
   
   def get_absolute_url(self):
     return reverse('finch-detail', kwargs={'finch_id': self.id})
+  
+class Feeding(models.Model):
+  date = models.DateField('Feeding date')
+  meal = models.CharField(
+    max_length=1,
+    choices=MEALS,
+    default=MEALS[0][0]
+  )
+
+  finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"{self.get_meal_display()} on {self.date}"
