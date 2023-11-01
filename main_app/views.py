661b9a0eefa073beb658fc23a89f01c32849033e
Maria Zhuravleva <contact.mzhuravleva@gmail.com>
2023-11-01 10:58:20 -0400

    create signup view function

--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
 from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from .models import Finch, Toy
 from .forms import FeedingForm
 
@@ -76,3 +78,17 @@ def remove_toy(request, finch_id, toy_id):
     toy = Toy.objects.get(id=toy_id)
     finch.toys.remove(toy)    
     return redirect('finch-detail', finch_id=finch_id)
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('cat-index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'signup.html', context)
